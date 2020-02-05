a172a8d0522d7e5db2d95951de47d363e70981a7
Hanne Lund <halu@aspit.dk>
2020-02-05 09:55:36 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>PHP opgaver</title>
+</head>
+<body>
+    <h1>PHP opgaver</h1>
+
+    <?php 
+        include "opgave8.php"; 
+        include "opgave10.php"
+    ?>
+</body>
+</html>
---
+++ opgave8.php
@@ -0,0 +1,59 @@
+<?php
+    $string = "the qick brown fox jumped over the lazy dog";
+
+    $word = "brown";
+
+    $email = "halu@aspit.dk";
+
+    $pwstring ="1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz";
+
+    function generatePw($length, $string)
+    {
+        return substr(str_shuffle($string), 0, $length);
+    }
+
+    function palindrome($testword)
+    {
+        if ($testword === strrev($testword))
+        {
+            return "$testword er et palindrom";
+        }
+        else
+        {
+            return "$testword er ikke et palindrom";
+        }
+    }
+?>
+
+<h2>Opgave 8</h2>
+
+    <p><?php echo strtoupper($string); ?></p>
+
+    <p><?php echo ucfirst($string); ?></p>
+
+    <p><?php echo ucwords($string); ?></p>
+
+    <p>
+        <?php 
+            if (strpos($string, $word))
+            {
+                echo "$word findes i strengen!";
+            }
+            else
+            {
+                echo "$word findes ikke i strengen!";
+            }
+        ?>
+    </p>
+
+    <p>
+        <?php
+            $exploded = explode("@", $email);
+
+            echo $exploded[0];
+        ?>
+    </p>
+
+    <p>Dit nye password er: <?php echo generatePw(8, $pwstring); ?></p>
+
+    <p><?php echo palindrome("regningen"); ?></p>
