50325536daf78c5e668d77edaa35e61d9e6492d2
AspIT-Hanne <halu@aspitdk>
2025-01-09 07:38:58 +0100

    Added New York site for embedded php training

---
+++ Eksamen/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eksamensopgaver</title>
+</head>
+<body>
+    <h1>Eksamensopgaver</h1>
+
+    <p><a href="V31-M1-E2021_StarupUIF_origfiler/index.php">Starup UIF</a></p>
+    <p><a href="V31-M3-2020/UdenPHP/index.php">Hansel and Petal flowers</a></p>
+    
+</body>
+</html>
