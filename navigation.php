cf4bc482565acdc15d5e50d48021af234bfe6f15
Hanne Lund <halu@aspit.dk>
2025-10-06 10:35:15 +0200

    Slettet undervisningsfiler

--- navigation.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Forside</a></li>
-        <li><a href="bagvaerk.php">Bagværk</a></li>
-        <li><a href="andre.php">Andre opskrifter</a></li>
-    </ul>
-</nav>
